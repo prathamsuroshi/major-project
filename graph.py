eec23eae415f450415546ef11921378ff9dd7168
prathamsuroshi <psuroshi9@gmail.com>
2024-02-22 01:07:40 +0530

    my commit

---
+++ graph.py
@@ -0,0 +1,17 @@
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# # Read the CSV file into a DataFrame
+# df = pd.read_csv('C:\Users\ASUS\Resume-Parser-Shortlisting-Project\files\outputs\Candidates.csv')
+
+# # # Example: Bar chart for the top 10 locations
+# # top_locations = df['Location'].value_counts().nlargest(10)
+# # top_locations.plot(kind='bar', title='Top 10 Locations')
+# # plt.xlabel('Location')
+# # plt.ylabel('Count')
+# # plt.show()
+
+# # Example: Pie chart for skills distribution
+# skills_distribution = df['Skills'].explode().value_counts()
+# skills_distribution.plot(kind='pie', title='Skills Distribution', autopct='%1.1f%%')
+# plt.show()
